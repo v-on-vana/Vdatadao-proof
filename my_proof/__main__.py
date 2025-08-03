709ca5fda03f5af180192ec2717375f2a5ffb218
Anonim <anon@domain.com>
2025-08-03 18:44:12 +0300

    Change DLP_ID

--- my_proof/__main__.py
+++ my_proof/__main__.py
@@ -8,15 +8,18 @@ import zipfile
 from my_proof.proof import Proof
 from my_proof.config import settings
 
-logging.basicConfig(level=logging.INFO, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
 
 def run() -> None:
     """Generate proofs for all input files."""
     # logging.info(f"Using config: {settings.model_dump_json(indent=2)}")
-    
-    settings.DLP_ID = 41 # TODO: Set your own DLP ID here
 
-    input_files_exist = os.path.isdir(settings.INPUT_DIR) and bool(os.listdir(settings.INPUT_DIR))
+    settings.DLP_ID = 143  # TODO: Set your own DLP ID here
+
+    input_files_exist = os.path.isdir(settings.INPUT_DIR) and bool(
+        os.listdir(settings.INPUT_DIR)
+    )
 
     if not input_files_exist:
         raise FileNotFoundError(f"No input files found in {settings.INPUT_DIR}")
@@ -26,7 +29,7 @@ def run() -> None:
     proof_response = proof.generate()
 
     output_path = os.path.join(settings.OUTPUT_DIR, "results.json")
-    with open(output_path, 'w') as f:
+    with open(output_path, "w") as f:
         json.dump(proof_response.model_dump(), f, indent=2)
     logging.info(f"Proof generation complete: {proof_response}")
 
@@ -37,7 +40,7 @@ def extract_input() -> None:
         input_file = os.path.join(settings.INPUT_DIR, input_filename)
 
         if zipfile.is_zipfile(input_file):
-            with zipfile.ZipFile(input_file, 'r') as zip_ref:
+            with zipfile.ZipFile(input_file, "r") as zip_ref:
                 zip_ref.extractall(settings.INPUT_DIR)
 
 
